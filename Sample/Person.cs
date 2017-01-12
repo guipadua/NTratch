feae310636b4bc9f5f40d073dc20ed8b53bbcfc3
guipadua <guipadua@live.com>
2017-01-12 17:52:21 -0500

    move and make a project of sample - fix bugs due to lack of binding

---
+++ Sample/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace NTratch.test
+{
+    class Person
+    {
+        string name;
+
+        public Person(string p_name)
+        {
+            name = p_name;
+            try
+            {
+                m1(); // possible exceptions: COMException, and 5 from System method GetFullPath: PathTooLongException, ArgumentException, SecurityException, ArgumentNullException, NotSupportedException
+                m2(); // possible exceptions: AccessViolationException, IOException
+                m2(); // possible exceptions: AccessViolationException, IOException
+                m3(-105); // possible exceptions: NotImplementedException
+                //m2(); // possible exceptions: AccessViolationException, IOException
+                //m30(); // pe: ---   none, it gets swallowed
+            }
+            catch (PathTooLongException ex)
+            {
+                //I'm the catch 22
+                Console.WriteLine("someone tried to load a path which is too long!" + ex.Message);
+                try
+                {
+                    Console.WriteLine("inner try: " + ex.Message);
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine("inner catch" + ex.Message);
+                }
+            }
+            catch (InternalBufferOverflowException ex)
+            {
+                //I'm the catch 22
+                Console.WriteLine("someone tried to load a path which is too long!" + ex.Message);
+            }
+            finally
+            {
+                PersonExtra.m20();
+            }
+        }
+        public void m1()
+        {
+            PersonExtra.m20(); // pe: InternalBufferOverflowException, AggregateException
+            PersonExtra.m30(); //pe: Exception
+            //s40 - system method that will throw something - path too long exception
+            //pe: PathTooLongException, ArgumentException, SecurityException, ArgumentNullException, NotSupportedException
+            Path.GetFullPath("I'm toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long for Windows File System to handle.");
+        }
+
+
+        private void m2()
+        {
+            try
+            {
+                //give me the ring! and
+                PersonExtra.m600();
+                throw new AccessViolationException(); // I will escape that catch! ha!
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine("mama, she called that long file again!");
+                // I will tell mama if you do that again 
+            }
+        }
+        private int m3(int x)
+        {
+            if (x > 0) { return x; };
+            return m3(x + 1);
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ Sample/PersonExtra.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace NTratch.test
+{
+    class PersonExtra
+    {
+        /// <exception cref="System.AggregateException">Always throw & have a invalid xml char.</exception>
+        public static void m20()
+        {
+            try
+            {
+                m500();
+                m600();
+                throw new AggregateException();
+
+            } catch(InternalBufferOverflowException boex)
+            {
+
+            }
+        }
+        public static void m500()
+        {
+            //cool 
+        }
+        public static  void m600()
+        {
+            throw new InternalBufferOverflowException();
+
+        }
+        public static void m30()
+        {
+            try
+            {
+                //give me the ring! and
+                m600();
+                throw new ArithmeticException();
+            }
+            catch
+            {
+                // don't tell mama anything
+                throw;
+            }
+        }
+    }
+}
